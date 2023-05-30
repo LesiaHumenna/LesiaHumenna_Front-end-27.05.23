47c40783d9dd0a1aa9407b96a7cd56e3e42376da
Lesia Humenna <lgumenna61@gmail.com>
2023-05-30 22:50:10 +0200

    tasks 2-lessons

---
+++ Front-end-Pro/index.js/script.js
@@ -0,0 +1,52 @@
+// prompt, alert 
+let myName = prompt("What's your name?")
+alert(`Hello, ${myName}! How are you?`);
+//скрипти на складання двох чисел
+let num1 = 5;
+let num2 = 10;
+let sum1 = num1 + num2;
+console.log(sum1);
+
+let num3 = 20;
+let num4 = 10;
+let sum2 = num3 - num4;
+console.log(sum2);
+
+let num5 = 5;
+let num6 = 10;
+let sum3 = num5 * num6;
+console.log(sum3);
+
+let num7 = 5;
+let num8 = 10;
+let sum4 = num7 / num8;
+console.log(sum4)
+//скрипт на true, false
+let str = "Hello";
+let num = 35;
+    if (str === num) {
+        console.log(true);
+    }
+    else{
+        console.log(false);
+    }
+// визначити середнє арифметичне
+let number1 = 5;
+let number2 = 10;
+let number3 = 15;
+
+let sumNum = (number1 + number2 + number3);
+console.log(sumNum);
+// розкласти 5-ти значне число
+let sumNumers = 30000;
+
+let modul1 = Math.floor(sumNumers / 10);
+let modul2 = Math.floor(sumNumers / 100);
+let modul3 = Math.floor(sumNumers / 1000);
+let modul4 = Math.floor(sumNumers / 10000);
+let modul5 = Math.floor(sumNumers / 100000);
+console.log(modul1, modul2, modul3, modul4, modul5);
+
+console.log(sumNumers);
+console.log('3 ' + '0 ' + '0 ' + '0 ' + '0');
+console.log('3 ', '0 ', '0 ', '0 ', '0');
